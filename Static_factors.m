cb05057e6a733156ed3a31a1183ecdaf97cbf009
bilelammouri <69475326+bilelammouri@users.noreply.github.com>
2021-08-02 15:47:33 +0100

    Add files via upload

---
+++ Static_factors.m
@@ -0,0 +1,24 @@
+function [F_PCA] = Static_factors(X,r);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This function compute and determine the Static Factors
+% Author : Ammouri Bilel
+% E-mail : bilel.ammouri@gmail.com
+% INPUTS
+% X - Matrix of observable variables (TxN)
+% r - Number of static factors
+%
+% OUTPUTS
+% F - Static facotrs
+% chi - Commun component gamma_chap * Ft   (T x N)
+% v - Eigenvector matrix
+% d - Enginevalue matrix
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+OPTS.disp = 0;
+[x] = Transform_data(X);
+[v, d] = eigs(cov(x),r,'lm',OPTS); 
+
+chi = x*v*v';
+d = eye(r);
+F_PCA=x*v; 
